fbfa6e11738fa9e779b6538dc6771c8c3b368c9c
Hugo Pina <hugopina.2000@gmail.com>
2022-03-31 12:42:17 -0300

    Changed to different pages mode. Home page done

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-const button = document.getElementById('navBtn');
+const home = document.getElementById('home');
+const about = document.getElementById('about');
+const work = document.getElementById('work');
+const contact = document.getElementById('contact');
 
-button.onclick = () => document.getElementById('about').scrollIntoView();
+const transitionToPage = (link) => {
+    document.querySelector('body').style.opacity = 0;
+    setTimeout(() => window.location.href = link , 1000);
+}
+
+document.addEventListener('DOMContentLoaded', function(event) {
+    document.querySelector('body').style.opacity = 1
+})
+
+home.onclick = () => transitionToPage('./index.html')
+about.onclick = () => transitionToPage('./about.html')
+work.onclick = () => transitionToPage('./work.html')
+contact.onclick = () => transitionToPage('./contact.html')
