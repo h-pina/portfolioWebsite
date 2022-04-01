6211a3e36d0acedffa24c4f4382e34a3792d7eb6
Hugo Pina <hugopina.2000@gmail.com>
2022-03-31 22:45:44 -0300

    About me basic layout done!

--- index.js
+++ index.js
@@ -3,16 +3,7 @@ const about = document.getElementById('about');
 const work = document.getElementById('work');
 const contact = document.getElementById('contact');
 
-const transitionToPage = (link) => {
-    document.querySelector('body').style.opacity = 0;
-    setTimeout(() => window.location.href = link , 1000);
-}
-
-document.addEventListener('DOMContentLoaded', function(event) {
-    document.querySelector('body').style.opacity = 1
-})
-
-home.onclick = () => transitionToPage('./index.html')
-about.onclick = () => transitionToPage('./about.html')
-work.onclick = () => transitionToPage('./work.html')
-contact.onclick = () => transitionToPage('./contact.html')
+home.onclick = () => window.location.href = './index.html';
+about.onclick = () => window.location.href = './about.html';
+work.onclick = () => window.location.href = './work.html';
+contact.onclick = () => window.location.href = './contact.html';
